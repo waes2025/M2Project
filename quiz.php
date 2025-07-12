fb4df3b2d80f3733d5b854a48dd7f185273930ba
waes2025 <waes_ahmed@yahoo.com>
2025-07-13 03:02:59 +0600

    quize upload

--- quiz.php
+++ quiz.php
@@ -11,5 +11,33 @@ $answers = [];
 
 foreach($questions as $index => $question)
 {
-    
+    echo ($index + 1 ). "." .$question['questions']. "\n";
+    $answers[] = trim(readline("Your answer: "));
+}
+
+function evaluteQuiz(array $questions, array $answers):int
+{
+    $score = 0;
+    foreach($questions as $index => $question)
+    {
+        if($answers[$index] === $question['correct']){
+            $score++;
+        }
+    }
+
+    //var_dump($score);
+
+    return $score;
+}
+
+$myScore = evaluteQuiz($questions, $answers);
+
+echo "\n Your scored $myScore out of ". count($questions). ".\n";
+
+if($myScore === count($questions)){
+    echo "Excelent Job! \n";
+}elseif($myScore >= 1){
+    echo "Good efford! \n";
+}else{
+    echo "Your result is fail, try again later. \n";
 }
